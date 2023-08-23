3beabe87c4fba358ba5c7c40e89fa835d8fa63f0
Bahadir UNGOR <bahadirungor@gmail.com>
2023-08-24 01:55:59 +0300

    v2 finished

--- script.js
+++ script.js
@@ -1,21 +1,82 @@
-function setTheme() {
+const themeUtil = (function () {
     const root = document.documentElement;
     const themeBtn = root.querySelector(".tt");
-    root.className === 'dark' ? root.removeAttribute('class') : root.className = 'dark';
-    themeBtn.textContent == "☀️" ? themeBtn.textContent ="🌙" : themeBtn.textContent ="☀️"
-}
-
-document.querySelector('.tt').addEventListener('click', setTheme)
-
-const gameBoard = document.querySelector(".gameBoard")
-
-function refresh(){
-    gameBoard.innerHTML = "";
-    for (let i = 0; i < 9; i++) {
-        const sqaure = document.createElement('div')
-        sqaure.classList.add('square')
-        gameBoard.appendChild(sqaure)
+    function toggle(){
+        root.className === 'dark' ? root.removeAttribute('class') : root.className = 'dark';
+        themeBtn.textContent == "☀️" ? themeBtn.textContent ="🌙" : themeBtn.textContent ="☀️"
     }
-}
+    return {toggle}
+})();
+
+document.querySelector('.tt').addEventListener('click', themeUtil.toggle)
+
+let turn = "X"
+
+const playRound = (function (){
+    const status = document.querySelector(".status")
+    return function(){
+        this.textContent = turn
+        turn == "X" ? turn = "O": turn = "X";
+        status.textContent = `Player ${turn}'s Turn:`
+        checkWinner()
+    }
+})();
+
+const refresh = (function (){
+    const gameBoard = document.querySelector(".gameBoard")
+    return function(){
+        turn = "X"
+        document.querySelector(".status").textContent = `Player ${turn}'s Turn:`
+        gameBoard.innerHTML = "";
+        for (let i = 0; i < 9; i++) {
+            const sqaure = document.createElement('div')
+            sqaure.classList.add('square')
+            sqaure.addEventListener('click', playRound)
+            gameBoard.appendChild(sqaure)
+        }
+    }
+})();
 
 refresh()
+
+document.querySelector(".restart").onclick = () => location.reload();
+
+const checkWinner = (function(){
+    const gameBoard = Array.from(document.querySelectorAll(".square"))
+    return function(){
+        let deneme = gameBoard.map(square => square.textContent)
+        if (deneme[0] + deneme[1] + deneme[2] == "XXX" ||
+            deneme[3] + deneme[4] + deneme[5] == "XXX" ||
+            deneme[6] + deneme[7] + deneme[8] == "XXX" ||
+            deneme[0] + deneme[3] + deneme[6] == "XXX" ||
+            deneme[1] + deneme[4] + deneme[7] == "XXX" ||
+            deneme[2] + deneme[5] + deneme[8] == "XXX" ||
+            deneme[0] + deneme[4] + deneme[8] == "XXX" ||
+            deneme[2] + deneme[4] + deneme[6] == "XXX"){
+                declareWinner("X")
+            } else if(deneme[0] + deneme[1] + deneme[2] == "OOO" ||
+            deneme[3] + deneme[4] + deneme[5] == "OOO" ||
+            deneme[6] + deneme[7] + deneme[8] == "OOO" ||
+            deneme[0] + deneme[3] + deneme[6] == "OOO" ||
+            deneme[1] + deneme[4] + deneme[7] == "OOO" ||
+            deneme[2] + deneme[5] + deneme[8] == "OOO" ||
+            deneme[0] + deneme[4] + deneme[8] == "OOO" ||
+            deneme[2] + deneme[4] + deneme[6] == "OOO"){
+                declareWinner("O")
+            }
+    }
+})();
+
+const declareWinner = (function(){
+    const gameBoard = document.querySelectorAll(".square")
+    const status = document.querySelector(".status")
+    return function(symbol){
+        gameBoard.forEach(square => square.removeEventListener("click", playRound))
+        status.textContent = "GAME OVER. WINNER IS PLAYER: " + symbol
+    }
+})();
+
+
+
+
+
